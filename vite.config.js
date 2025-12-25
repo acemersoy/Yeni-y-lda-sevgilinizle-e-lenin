e4b21deed8d6ae9f9cebc046b39f4a44a1057311
avnicem <a.cem.ersoy@hotmail.com>
2025-12-25 18:32:54 +0300

    fix: react global in pages build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/Yeni-y-lda-sevgilinizle-e-lenin/'
-})
+  base: "/Yeni-y-lda-sevgilinizle-e-lenin/",
+  plugins: [react()],
+});
